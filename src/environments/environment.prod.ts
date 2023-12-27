21b3803cd3f4df92df7a718cb265ae323418c7bb
eve seni <senieve@gmail.com>
2023-12-27 16:47:51 -0500

    Untrack environments folder

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    apiKey: 'f4562e002d51f8936edd00913250fdc2',
-
-  };
-  //test    
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiKey: 'f4562e002d51f8936edd00913250fdc2',
-
-  };
